3526ca50354092b9541acf41dfdbe8b74a5cfa3b
jswj30 <jswj30@naver.com>
2021-05-13 19:40:19 +0900

    [feat5][feature]로그인 기능 만드는 중

--- mytodo-client/src/App.js
+++ mytodo-client/src/App.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Login from "./Login";
 import Main from "./Main";
+const axios = require("axios");
 
-const App = () => {
-  return (
-    <div>
-      <Route path="/" component={Login} exact />
-      <Route path="/main" component={Main} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false,
+      id: "",
+      email: "",
+      name: "",
+      mobile: "",
+    };
+  }
+
+  handleLogin = async () => {
+    try {
+      const resp = await axios({
+        method: "post",
+        url: "http://localhost:5000/signin",
+        data: {
+          email: "test@test.com",
+          password: "test",
+        },
+      });
+      this.setState({
+        isLogin: !this.state.isLogin,
+        id: resp.id,
+        email: resp.email,
+        name: resp.name,
+        mobile: resp.mobile,
+      });
+      this.props.history.push("/main");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  render() {
+    return (
+      <div>
+        <Route
+          path="/"
+          component={Login}
+          exact
+          handleLogin={this.handleLogin}
+        />
+        <Route path="/main" component={Main} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- mytodo-client/src/Login.js
+++ mytodo-client/src/Login.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Login = () => {
-  return (
-    <div className="login">
-      <div className="title">
-        <h1>MyTodo</h1>
+class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <div className="title">
+          <h1>MyTodo</h1>
+        </div>
+        <div className="login_info">
+          <h3>Email</h3>
+          <input type="text" placeholder="Email을 입력하세요." />
+          <h3>Email</h3>
+          <input type="password" placeholder="Password를 입력하세요." />
+        </div>
+        <div className="login_submit">
+          <button className="loginButton" onClick={() => alert("로그인")}>
+            로그인
+          </button>
+          <button className="testButton" onClick={() => this.props.handleLogin}>
+            체험판 로그인
+          </button>
+        </div>
       </div>
-      <div className="login_info">
-        <h3>Email</h3>
-        <input type="text" placeholder="Email을 입력하세요." />
-        <h3>Email</h3>
-        <input type="password" placeholder="Password를 입력하세요." />
-      </div>
-      <div className="login_submit">
-        <button className="loginButton" onClick={() => alert("로그인")}>
-          로그인
-        </button>
-        <button className="testButton" onClick={() => alert("로그인")}>
-          체험판 로그인
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Login;
