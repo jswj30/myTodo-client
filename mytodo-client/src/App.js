a8f4115ba424feafa8d9bb5f203248ce7500fd44
jswj30 <jswj30@naver.com>
2021-05-14 14:38:52 +0900

    [feat5][feature]로그인 기능 생성

--- mytodo-client/src/App.js
+++ mytodo-client/src/App.js
@@ -1,53 +1,39 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+
+// components
 import Login from "./Login";
 import Main from "./Main";
-const axios = require("axios");
+
+// axios
+// const axios = require("axios");
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLogin: false,
-      id: "",
-      email: "",
-      name: "",
-      mobile: "",
     };
   }
 
-  handleLogin = async () => {
-    try {
-      const resp = await axios({
-        method: "post",
-        url: "http://localhost:5000/signin",
-        data: {
-          email: "test@test.com",
-          password: "test",
-        },
-      });
-      this.setState({
-        isLogin: !this.state.isLogin,
-        id: resp.id,
-        email: resp.email,
-        name: resp.name,
-        mobile: resp.mobile,
-      });
-      this.props.history.push("/main");
-    } catch (err) {
-      console.error(err);
-    }
+  handleIsLogin = () => {
+    this.setState({
+      isLogin: !this.state.isLogin,
+    });
   };
+
   render() {
+    const { isLogin } = this.state;
     return (
       <div>
         <Route
           path="/"
-          component={Login}
           exact
-          handleLogin={this.handleLogin}
+          render={() =>
+            isLogin ? <Main /> : <Login handleIsLogin={this.handleIsLogin} />
+          }
         />
-        <Route path="/main" component={Main} />
+        {/* <Route path="/main" component={Main} /> */}
       </div>
     );
   }
