d9684f1cb84342982aedc527ae4fa91ab9487197
jswj30 <jswj30@naver.com>
2021-05-19 19:55:24 +0900

    [feat21][feature]/main에 todo 추가 태그 만들기

--- mytodo-client/src/components/Main.js
+++ mytodo-client/src/components/Main.js
@@ -9,16 +9,44 @@ axios.defaults.withCredentials = true;
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputTodo: "",
+    };
   }
+
+  // todo 입력값을 setState
+  handleSetStateTodo = (e) => {
+    this.setState({
+      inputTodo: e.target.value,
+    });
+  };
+
+  // todo 등록하기
+  handleIputTodo = () => {
+    alert(this.state.inputTodo);
+  };
 
   render() {
     let { todoInfo, handleSignout } = this.props;
     return (
       <div>
-        <h1>메인페이지</h1>
-        {todoInfo &&
-          todoInfo.map((todo, i) => <TodoList todo={todo} key={i} />)}
-        <button onClick={() => handleSignout()}>로그아웃</button>
+        <h1>TodoList</h1>
+        <div className="inputTodo">
+          <input
+            type="text"
+            placeholder="Todo를 입력해주세요."
+            onChange={(e) => this.handleSetStateTodo(e)}
+            value={this.state.inputTodo}
+          />
+          <button className="submitTodo" onClick={() => this.handleIputTodo()}>
+            등록
+          </button>
+        </div>
+        <div className="todolist">
+          {todoInfo &&
+            todoInfo.map((todo, i) => <TodoList todo={todo} key={i} />)}
+          <button onClick={() => handleSignout()}>로그아웃</button>
+        </div>
       </div>
     );
   }
