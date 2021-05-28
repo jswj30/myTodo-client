9037cf2b72598e605ad59d36ff878ac469b2913b
jswj30 <jswj30@naver.com>
2021-05-28 13:25:30 +0900

    [feat61][feature]todolist 중요 체크박스 추가

--- mytodo-client/src/components/TodoList.js
+++ mytodo-client/src/components/TodoList.js
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./TodoList.scss";
 import { MdDeleteForever } from "react-icons/md";
+import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 
 // axios
 const axios = require("axios");
 axios.defaults.withCredentials = true;
 
 class TodoList extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false,
+    };
+  }
+
+  // 중요 체크하기
+  handleImportant = () => {
+    this.setState({
+      important: !this.state.important,
+    });
+  };
 
   // todo 삭제하기
   handleRemoveTodo = () => {
@@ -31,8 +42,12 @@ class TodoList extends Component {
 
   render() {
     const { content } = this.props.todo;
+    const { important } = this.state;
     return (
       <div className="todolist">
+        <span className="todolist_checkbox" onClick={this.handleImportant}>
+          {important ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />}
+        </span>
         <span className="todolist_content">{content}</span>
         <MdDeleteForever
           className="deleteIcon"
