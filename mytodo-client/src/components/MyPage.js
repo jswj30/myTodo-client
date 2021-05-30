3f97f286b581b0a84d2a6956a6ef152eeea16291
jswj30 <jswj30@naver.com>
2021-05-30 13:24:08 +0900

    [feat79][feature]회원정보 수정 기능 생성

--- mytodo-client/src/components/MyPage.js
+++ mytodo-client/src/components/MyPage.js
@@ -13,28 +13,100 @@ class MyPage extends Component {
       editPW: false,
       editName: false,
       editMobile: false,
+      inputPW: "",
+      inputName: this.props.userInfo.name,
+      inputMobile: this.props.userInfo.mobile,
     };
   }
 
-  // PW 수정 input 변환
-  handleEditPW = () => {
+  // PW 수정 input으로 변환
+  handleOpenEditPW = () => {
     this.setState({
-      editPW: !this.state.editPW,
+      editPW: true,
     });
+  };
+
+  // PW 정보 수정 + PW test로 변환
+  handleCloseEditPW = () => {
+    axios
+      .patch("http://localhost:5000/edituser", {
+        id: this.props.userId,
+        password: this.state.inputPW,
+        name: this.state.inputName,
+        mobile: this.state.inputMobile,
+      })
+      .then((result) => {
+        this.setState({
+          editPW: false,
+          inputPW: result.data.password,
+          inputName: result.data.name,
+          inputMobile: result.data.mobile,
+        });
+        this.props.handleIsMyPage(result);
+      })
+      .catch((err) => {
+        alert("정보가 수정되지 않았습니다.");
+      });
   };
 
   // Name 수정 input 변환
-  handleEditName = () => {
+  handleOpenEditName = () => {
     this.setState({
-      editName: !this.state.editName,
+      editName: true,
     });
   };
 
+  // Name 정보 수정 + Name test로 변환
+  handleCloseEditName = () => {
+    axios
+      .patch("http://localhost:5000/edituser", {
+        id: this.props.userId,
+        password: this.props.userInfo.password,
+        name: this.state.inputName,
+        mobile: this.state.inputMobile,
+      })
+      .then((result) => {
+        this.setState({
+          editName: false,
+          inputPW: result.data.password,
+          inputName: result.data.name,
+          inputMobile: result.data.mobile,
+        });
+        this.props.handleIsMyPage(result);
+      })
+      .catch((err) => {
+        alert("정보가 수정되지 않았습니다.");
+      });
+  };
+
   // Mobile 수정 input 변환
-  handleEditMobile = () => {
+  handleOpenEditMobile = () => {
     this.setState({
-      editMobile: !this.state.editMobile,
+      editMobile: true,
     });
+  };
+
+  // Mobile 정보 수정 + Name test로 변환
+  handleCloseEditMobile = () => {
+    axios
+      .patch("http://localhost:5000/edituser", {
+        id: this.props.userId,
+        password: this.props.userInfo.password,
+        name: this.state.inputName,
+        mobile: this.state.inputMobile,
+      })
+      .then((result) => {
+        this.setState({
+          editMobile: false,
+          inputPW: result.data.password,
+          inputName: result.data.name,
+          inputMobile: result.data.mobile,
+        });
+        this.props.handleIsMyPage(result);
+      })
+      .catch((err) => {
+        alert("정보가 수정되지 않았습니다.");
+      });
   };
 
   // 뒤로 가기 기능
@@ -42,6 +114,29 @@ class MyPage extends Component {
     this.props.handleStopIsMyPage();
     this.props.history.push("/main");
   };
+
+  // PW 입력값을 setState
+  handleSetStatePW = (e) => {
+    this.setState({
+      inputPW: e.target.value,
+    });
+  };
+
+  // name 입력값을 setState
+  handleSetStateName = (e) => {
+    this.setState({
+      inputName: e.target.value,
+    });
+  };
+
+  // name 입력값을 setState
+  handleSetStateMobile = (e) => {
+    this.setState({
+      inputMobile: e.target.value,
+    });
+  };
+
+  //
 
   render() {
     let { userInfo, handleSignout } = this.props;
@@ -54,45 +149,87 @@ class MyPage extends Component {
           <div className="myPage_text">{userInfo.email}</div>
           <div className="myPage_password">
             Password
-            <button className="myPage_editBtn" onClick={this.handleEditPW}>
-              수정
-            </button>
+            {editPW ? (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleCloseEditPW}
+              >
+                완료
+              </button>
+            ) : (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleOpenEditPW}
+              >
+                수정
+              </button>
+            )}
           </div>
           {editPW ? (
             <input
               className="inputMyPage"
               type="password"
               placeholder="Password을 입력하세요."
+              onChange={(e) => this.handleSetStatePW(e)}
+              value={this.state.inputPW}
             />
           ) : (
-            <div className="myPage_text">{userInfo.password}</div>
+            <div className="myPage_text">**********</div>
           )}
           <div className="myPage_name">
             Name
-            <button className="myPage_editBtn" onClick={this.handleEditName}>
-              수정
-            </button>
+            {editName ? (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleCloseEditName}
+              >
+                완료
+              </button>
+            ) : (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleOpenEditName}
+              >
+                수정
+              </button>
+            )}
           </div>
           {editName ? (
             <input
               className="inputMyPage"
               type="text"
               placeholder="이름을 입력하세요."
+              onChange={(e) => this.handleSetStateName(e)}
+              value={this.state.inputName}
             />
           ) : (
             <div className="myPage_text">{userInfo.name}</div>
           )}
           <div className="myPage_mobile">
             Mobile
-            <button className="myPage_editBtn" onClick={this.handleEditMobile}>
-              수정
-            </button>
+            {editMobile ? (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleCloseEditMobile}
+              >
+                완료
+              </button>
+            ) : (
+              <button
+                className="myPage_editBtn"
+                onClick={this.handleOpenEditMobile}
+              >
+                수정
+              </button>
+            )}
           </div>
           {editMobile ? (
             <input
               className="inputMyPage"
               type="text"
               placeholder="전화번호를 입력하세요."
+              onChange={(e) => this.handleSetStateMobile(e)}
+              value={this.state.inputMobile}
             />
           ) : (
             <div className="myPage_text">{userInfo.mobile}</div>
